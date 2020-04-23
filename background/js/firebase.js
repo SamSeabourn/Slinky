5d1a8a15986e2c63d500685425700596bf6f9520
Sam Seabourn <sam.seabourn@gmail.com>
2020-04-23 12:36:29 +1000

    Delete from firebase working

--- background/js/firebase.js
+++ background/js/firebase.js
@@ -1,28 +1,111 @@
 
 var currentUser = new User;
 var bookmarkDB;
-let usersBookmarks = [];
+let usersBookmarks = []; //Caching user bookmarks
 
 firebase.initializeApp(firebaseConfig);
 
-
 firebase.auth().signInAnonymously().catch(function (error) {
-    console.log(error.code)
-    console.log(error.message)
+    //Todo Error handling
 });
 
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         currentUser.isAnonymous = user.isAnonymous;
         currentUser.uId = user.uid;
-        currentUser.refreshToken = user.refreshToken
-        setFirebaseUpdateListener()
+        currentUser.refreshToken = user.refreshToken;
+        initalizeFirebaseUpdateListener();
     } else {
-        console("Auth Error")
+        console("Authentication Error")
     }
 });
 
-/// Waiting on post from popup.jj
+function saveData(newBookmark) {         //This will update existing bookmark with new data
+    if (urlAlreadyExists(newBookmark.url)) {
+        updateBookmark(getKeyFromUrl(newBookmark.url), newBookmark)
+    } else {
+        bookmarkDB.push(newBookmark);
+    }
+}
+
+function updateBookmark(bookmarkId, updatedBookmark) {
+    bookmarkDB.child(bookmarkId).set(updatedBookmark);
+}
+
+function initalizeFirebaseUpdateListener() {
+    // Needs to rebuild the object from Firebase to cache
+    function gotData(data) {
+        usersBookmarks = [];
+        var bookmarks = data.val()
+        var keys = Object.keys(bookmarks)
+        for (var i = 0; i < keys.length; i++) {
+            var k = keys[i]
+            if (!bookmarks[k].isDeleted && ) {
+
+                var bookmark = new Bookmark(
+                    bookmarks[k].url,
+                    bookmarks[k].favicon,
+                    bookmarks[k].title,
+                    bookmarks[k].tags,
+                    bookmarks[k].clicks,
+                )
+                bookmark.bId = k
+                usersBookmarks.push(bookmark)
+            } else {
+                console.log(bookmarks[k].title + " is deleted")
+            }
+        }
+        // chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+        //     console.log(response.farewell);
+        // });
+    }
+
+    function errData(data) {
+        console.error("Error")
+        console.error(err)
+    }
+
+    bookmarkDB = firebase.database().ref(`/users/${currentUser.uId}/bookmarks`)
+
+    bookmarkDB.on('value', function (snapshot) {
+        if (snapshot.val() !== undefined)
+            bookmarkDB.on('value', gotData, errData)
+    })
+}
+
+function urlAlreadyExists(url) {
+    let exists;
+    bookmarkDB.orderByChild('url').equalTo(url).on("value", function (snapshot) {
+        exists = (snapshot.val() !== null)
+    })
+    return exists;
+}
+
+function getKeyFromUrl(url) {
+    let key;
+    bookmarkDB.orderByChild('url').equalTo(url).on("value", function (snapshot) {
+        exists = (snapshot.val() !== null)
+        key = Object.keys(snapshot.val())[0];
+    })
+    return key;
+}
+
+//Chrome runtime messaging
+
+// function sendUserBookmarksToContent(){
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+//         chrome.tabs.sendMessage(tabs[0].id, { data: usersBookmarks }, function(response) {
+//             console.log(response)
+//             console.log(response)
+//         });  
+//     });
+// }
+
+function deleteBookmark(bId) {
+    firebase.database().ref(`/users/${currentUser.uId}/bookmarks/${bId}`).update({ isDeleted: true });
+}
+
+//Waiting for data from the popup.js
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         var newBookmark = request;
@@ -30,90 +113,21 @@ chrome.runtime.onMessage.addListener(
         sendResponse({ message: "Sending data to DB" });
     });
 
-function saveData(newBookmark) {
-    if (urlAlreadyExists(newBookmark.url)){
-        bookmarkDB.child(getKeyFromUrl(newBookmark.url)).set(newBookmark);
-    } else {
-        bookmarkDB.push(newBookmark);
-    } 
-}
-
-function setFirebaseUpdateListener(){
-
-    // Needs to rebuild the object from Firebase
-    function gotData(data){
-        usersBookmarks = [];
-        var bookmarks = data.val()
-        var keys = Object.keys(bookmarks)
-        for (var i = 0; i < keys.length; i++ ){
-            var k = keys[i]
-            var bookmark = new Bookmark(
-                bookmarks[k].url,
-                bookmarks[k].favicon,
-                bookmarks[k].title,
-                bookmarks[k].tags,
-                bookmarks[k].clicks, 
-            )
-            bookmark.bId = k //Grabbing the key direct from firebase
-            usersBookmarks.push(bookmark)
-        }
-        console.log("this is the object from the DB braaah")
-        console.log(usersBookmarks);
-    }
-    
-    function errData(data){
-        console.error("Error")
-        console.error(err)
-    }
-
-    bookmarkDB = firebase.database().ref().child("users").child(currentUser.uId).child("bookmarks");
-    bookmarkDB.on('value', function(snapshot) {
-        if (snapshot.val() !== undefined)
-        bookmarkDB.on('value', gotData, errData)
-    })
-}
-
-
-function urlAlreadyExists(url){
-    let exists;
-    bookmarkDB.orderByChild('url').equalTo(url).on("value", function(snapshot) {
-        exists = (snapshot.val() !== null )
-    })
-    return exists;
-}
-
-function getKeyFromUrl(url){
-    let key;
-    bookmarkDB.orderByChild('url').equalTo(url).on("value", function(snapshot) {
-        exists = (snapshot.val() !== null )
-        key = Object.keys(snapshot.val())[0];
-    })
-    return key;
-}
-
-function sendUserBookmarksToContent(){
-        console.log("sending users bookmarks")
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id, { data: usersBookmarks }, function(response) {
-                console.log(response)
-                console.log(response)
-            });  
-        });
-    }
-
-
-
+//Waiting for requests from the JS   
 chrome.runtime.onMessageExternal.addListener(
-    function(request, sender, sendResponse) {
-      console.log(request.request)
-      if (request.message === "getAllbookmarks"){
-        sendResponse({data: usersBookmarks})
-        // sendUserBookmarksToContent()
-      }
-      else {
-        sendResponse({message: "wrong request G"})
-      } 
+    function (task, sender, sendResponse) {
+        switch (task.task) {
+            case "getAllbookmarks":
+                sendResponse({ data: usersBookmarks })
+                break;
+            case "deleteBookmark":
+                deleteBookmark(task.bId)
+                sendResponse({ data: usersBookmarks })
+            default:
+                sendResponse({ data: "BAD REQUEST BRAH" })
+        }
     });
+
 
 
 
