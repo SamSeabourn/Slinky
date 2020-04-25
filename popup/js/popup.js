601e2ea2056ad1fddeddae786b530aa9793f7417
Sam Seabourn <sam.seabourn@gmail.com>
2020-04-25 15:44:14 +1000

    almost finished the popup with tooltips

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -3,18 +3,21 @@ let tagInputElement;
 let tagContainerElement;
 let currentWebsiteTitleElement;
 let currentWebsiteImageElement;
-
 let currentWebsite = new Bookmark();
+let chromeCommands;
+let shortcutMessage;
+
+chrome.commands.getAll(function(commands){
+  chromeCommands = commands
+})
 
 document.addEventListener('DOMContentLoaded', function () {
-
   currentWebsiteTitleElement = document.getElementById('currentWebsiteTitleElement');
   currentWebsiteUrlElement = document.getElementById('currentWebsiteUrlElement');
   tagInputElement = document.getElementById('tagInputElement');
   tagContainerElement = document.getElementById('tagContainerElement');
   currentWebsiteImageElement = document.getElementById('currentWebsiteImageElement');
-
-  // tagInputElement.focus();
+  shortcutMessage = document.getElementById('shortcutMessage');
 
   chrome.tabs.getSelected(null, function (tab) {
     if (tab.url !== undefined || tab.url !== null) {
@@ -36,18 +39,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
 }, false);
 
-//REDO THIS SHIT AND THINK OF BETTER SHORTCUTS --> https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code
-document.addEventListener('keydown', checkKeys);
-function checkKeys(e) {
-  if (e.code === "Space" && tagInputElement.value != " ") {
+
+window.addEventListener("keydown", function(e) {
+  if (e.key === " " && tagInputElement.value != " ") updateTags(currentWebsite.tags);
+  if (e.key === "Enter") {
     updateTags(currentWebsite.tags);
-  }
-  if (e.code === "Enter") {
-    updateTags(currentWebsite.tags);
+    updateTitle();
     sendWebsiteToFirebase();
-    uploadCompleteMessage();
+    showUploadCompleteMessage();
   }
-}
+}, true);
 
 
 function updatePopup() {
@@ -81,45 +82,79 @@ function updateTags() {
   renderTags(currentWebsite.tags)
 }
 
+function updateTitle() {
+  currentWebsite.title = currentWebsiteTitleElement.value
+}
+
 
 function sendWebsiteToFirebase() {
   chrome.runtime.sendMessage(currentWebsite, function (response) {
   });
 }
 
-function uploadCompleteMessage() {
+function showUploadCompleteMessage() {
+  shortcutMessage.innerHTML = createShortcutString();
   var uploadCompleteDiv = document.getElementsByClassName('slinky-upload-complete')[0];
   var shownDivs = document.getElementsByClassName('fade-out')
-  debugger;
   for (let i = 0; i < shownDivs.length; i++) {
-    console.log(shownDivs[i])
     shownDivs[i].classList.remove('show')
     shownDivs[i].classList.add('hide');
   }
   uploadCompleteDiv.classList.remove('hidden')
   uploadCompleteDiv.classList.add('show')
+
 }
 
 function inputScroller(inputElement, maxCharacterCount) {
+  var increment = 1 
+  function waitTime(ms) {
+      increment = 0
+      setTimeout(function () { increment = 1 }, ms);
+  }
   var characterCount = inputElement.value.length;
-  var speed = (characterCount > (maxCharacterCount + 20) ? 30 : 100)
+  var speed = (characterCount > (maxCharacterCount + 5) ? 40 : 100)
+  var scrollRight = true
+  var scrollLeft  = false 
+  
   if (characterCount > maxCharacterCount) {
     var previousScrollPosition = -1 //intializer must be less than zero
     setInterval(function () {
       if (inputElement.scrollLeft > previousScrollPosition || inputElement.scrollLeft === 0) {
+        if (scrollRight) {
+          scrollRight = !scrollRight
+          scrollLeft = true
+          waitTime(3000)
+        }
         previousScrollPosition = inputElement.scrollLeft
-        inputElement.scrollLeft = inputElement.scrollLeft + 1
+        inputElement.scrollLeft = inputElement.scrollLeft + increment
       } else {
-        inputElement.scrollLeft = inputElement.scrollLeft - 1
+        if (scrollLeft) {
+          scrollLeft = !scrollLeft
+          scrollRight = true
+          waitTime(3000)
+        }
+        inputElement.scrollLeft = inputElement.scrollLeft - increment
       }
     }, speed );
   }
 }
 
-
-function updateShortCutInfo(){
-  var shortcutMessage = document.getElementById("tagInputElement")
+function createShortcutString(){
+  const openSlinkyKeys = chromeCommands[1].shortcut.split("+")
+  let resultString = "Press ";
+  for (let i = 0; i < openSlinkyKeys.length; i++) {
+    resultString = resultString + `<span class="short-cut-keys">${ openSlinkyKeys[i] }</span>`
+    if (i < openSlinkyKeys.length){
+      resultString = resultString + "+"
+    }
+  }
+  resultString = resultString + " to open your links"
+  return resultString
 }
+
+
+
+  
 
 
 
