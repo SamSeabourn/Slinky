6654e64ed1c28124d6621f4a31bf44a94f655aa0
Sam Seabourn <sam.seabourn@gmail.com>
2020-04-25 15:44:59 +1000

    Almost finished popup with tooltips

--- background/js/background.js
+++ background/js/background.js
@@ -1,14 +1,21 @@
-// console.log('hello from background')
-// ///chrome.tabs.create({url:"popup.html"});
-// /////////////////THIS IS HOW YOU GET THE BOOKMARK LIBRARY PAGE TO OPEN!!!
 
-// let slinkyViewerIsOpen = false;
+// This needs to check the tab isnt already open
+let slinkyIsOpen = false;
 
 chrome.commands.onCommand.addListener(function(command) {
-    if (command === "open-slinky" && !slinkyViewerIsOpen) {
-        chrome.tabs.create({url:"app/slinky-viewer/public/index.html"});
+    if (command === "open-slinky" && !slinkyIsOpen) {
+
+        chrome.tabs.create({url:"../../tab/app/index.html"});
+        var x = chrome.commands.getAll()
         slinkyViewerIsOpen = true;
+        console.log(x)
     } else {
         //TODO Close window 
     }
 });
+
+
+
+chrome.commands.getAll(function(commands){
+    console.log(commands)
+  })
--- background/js/firebase.js
+++ background/js/firebase.js
@@ -139,3 +139,5 @@ chrome.runtime.onMessageExternal.addListener(
 
 
 
+
+
